9121d36ceb2eda808482cfa8f8e45b7012a16d76
root <root@yuan.yuan.com>
2019-07-10 12:33:13 +0800

    add js2-mode and web-mode

---
+++ configs/web-conf.el
@@ -0,0 +1,34 @@
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
+
+(require 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist
+'("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
+
+(setq web-mode-enable-auto-pairing nil)
+
+(sp-with-modes '(web-mode)
+  (sp-local-pair "%" "%"
+                 :unless '(sp-in-string-p)
+                 :post-handlers '(((lambda (&rest _ignored)
+                                     (just-one-space)
+                                     (save-excursion (insert " ")))
+                                   "SPC" "=" "#")))
+  (sp-local-tag "%" "<% "  " %>")
+  (sp-local-tag "=" "<%= " " %>")
+  (sp-local-tag "#" "<%# " " %>"))
+
+
+
+(provide 'web-conf)
