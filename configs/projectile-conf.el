68caeba4aac6b416916f0abaa2d444fa263ed1c6
yuan.zhang <yuan.zhang@zhangmen.com>
2019-07-12 15:18:29 +0800

    add helm project

--- configs/projectile-conf.el
+++ configs/projectile-conf.el
@@ -3,9 +3,14 @@
 (setq projectile-cache-file (expand-file-name  "projectile.cache" savefile-dir))
 (projectile-mode t)
 (setq projectile-switch-project-action 'projectile-dired)
+(setq projectile-project-search-path '("~/disk/dev/zhangmen/"))
 
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p A") 'projectile-add-known-project)
+
+(require 'helm-projectile)
+(helm-projectile-on)
+(define-key projectile-mode-map (kbd "C-c p h") 'helm-projectile)
 
 (provide 'projectile-conf)
