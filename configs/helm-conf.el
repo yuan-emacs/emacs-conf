223557836599df16f107d72b73bcd82d24f72b17
zhangyuanyuan <yuan.zhang@zhangmen.com>
2019-07-12 23:30:15 +0800

    change M-x

--- configs/helm-conf.el
+++ configs/helm-conf.el
@@ -1,4 +1,6 @@
 (require 'helm-ag)
+(helm-mode t)
+(global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "S-SPC p f") 'helm-do-ag-project-root)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (provide 'helm-conf)
