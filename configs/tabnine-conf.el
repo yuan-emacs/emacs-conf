3740fe6a5071a5428963753ff0747263a8328b01
yuan.zhang <yuan.zhang@zhangmen.com>
2019-07-19 18:34:31 +0800

    优化一把 tabnine

--- configs/tabnine-conf.el
+++ configs/tabnine-conf.el
@@ -16,4 +16,14 @@
         company-pseudo-tooltip-frontend
         company-echo-metadata-frontend))
 
+;; The free version of TabNine is good enough,
+;; and below code is recommended that TabNine not always
+;; prompt me to purchase a paid version in a large project.
+(defadvice company-echo-show (around disable-tabnine-upgrade-message activate)
+  (let ((company-message-func (ad-get-arg 0)))
+    (when (and company-message-func
+               (stringp (funcall company-message-func)))
+      (unless (string-match "The free version of TabNine only indexes up to" (funcall company-message-func))
+        ad-do-it))))
+
 (provide 'tabnine-conf)
