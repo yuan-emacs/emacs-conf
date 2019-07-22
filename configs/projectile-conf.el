2874dc29fc67c6f7fad41cf21f77582343ed70fe
yuan.zhang <yuan.zhang@zhangmen.com>
2019-07-22 11:28:45 +0800

    add s-p for projectile-command-map and helm-prjectile

--- configs/projectile-conf.el
+++ configs/projectile-conf.el
@@ -8,9 +8,11 @@
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p A") 'projectile-add-known-project)
+(define-key projectile-mode-map (kbd "s-p A") 'projectile-add-known-project)
 
 (require 'helm-projectile)
 (helm-projectile-on)
 (define-key projectile-mode-map (kbd "C-c p h") 'helm-projectile)
+(define-key projectile-mode-map (kbd "s-p h") 'helm-projectile)
 
 (provide 'projectile-conf)
